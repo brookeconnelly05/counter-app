094853874d2631ca8e3243c739961ac1267846e5
brookeconnelly05 <bac6128@psu.edu>
2025-02-23 13:53:40 -0500

    modified:   counter-app.js 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json

--- counter-app.js
+++ counter-app.js
@@ -20,6 +20,9 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
+    this.count = 0;
+    this.max;
+    this.min;
     this.title = "";
     this.t = this.t || {};
     this.t = {
@@ -39,7 +42,10 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   static get properties() {
     return {
       ...super.properties,
-      title: { type: String },
+      count: {type: Number, reflect: true},
+      max: {type: Number},
+      min: {type: Number},
+
     };
   }
 
@@ -53,23 +59,89 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
+      :host([count="18"]) {
+          color: var(--ddd-theme-default-athertonViolet);
+      }
+      :host([count="21"]) {
+          color: var(--ddd-theme-default-skyBlue);
+      }
+
+      .counter.min {
+          color: var(--ddd-theme-default-creekTeal);
+        }
+        .counter.max {
+          color: var(--ddd-theme-default-futureLime);
+        }
+  
       .wrapper {
         margin: var(--ddd-spacing-2);
         padding: var(--ddd-spacing-4);
       }
-      h3 span {
-        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+     
+      .counter {
+        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-xxl));
       }
+      button:hover {
+          background-color: var(--ddd-theme-default-slateGray);
+        }
+        button:focus {
+          outline: 2px solid var(--ddd-theme-default-shrineMaxLight);
+        }
+      
     `];
+  }
+
+  updated(changedProperties){
+    super.updated(changedProperties)
+    if(changedProperties.has("count")){
+      console.log("count changed to:", this.count);
+      if(this.count ===21){
+        this.makeItRain();
+      }
+      
+    }
+  }
+
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <confetti-container id="confetti" class="wrapper">
+<div class="counter ${this.count === this.min ? 'min' : this.count === this.max ? 'max' : ''}">${this.count} </div>
+<div class="buttons">
+  <button @click= "${this.increase}" ?disabled="${this.count === this.max}">Add one </button>
+  <button @click= "${this.decrease}" ?disabled="${this.count === this.min}">Subtract one </button>
+  </div>
+</confetti-container>
+`;
+  }
+  increase(){
+    this.count++;
+  }
+  decrease(){
+    this.count--;
+  }
+  reset(){
+    this.count=0;
   }
 
   /**
